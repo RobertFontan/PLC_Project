fcce619445926cd7e699266cda885e98c160aea0
Jordan-Insinger <Jordan.insinger@ufl.edu>
2022-09-17 13:36:57 -0400

    Added the peek/match methods

--- src/main/java/plc/project/Lexer.java
+++ src/main/java/plc/project/Lexer.java
@@ -72,7 +72,16 @@ public final class Lexer {
      * return true if the next characters are {@code 'a', 'b', 'c'}.
      */
     public boolean peek(String... patterns) {
-        throw new UnsupportedOperationException(); //TODO (in Lecture)
+
+        for (int i = 0; i < patterns.length; i++) {
+
+            if (!chars.has(i) ||
+                !String.valueOf(chars.get(i)).matches(patterns[i])) {
+
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -81,7 +90,16 @@ public final class Lexer {
      * true. Hint - it's easiest to have this method simply call peek.
      */
     public boolean match(String... patterns) {
-        throw new UnsupportedOperationException(); //TODO (in Lecture)
+        boolean peek = peek(patterns);
+
+        if(peek) {
+
+            for (int i = 0; i < patterns.length; i++) {
+                chars.advance();
+            }
+
+        }
+        return peek;
     }
 
     /**
